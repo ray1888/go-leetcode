d9ac942df24baa756e6fbd3c46e02e9cb798e17a
ray1888 <alal1995@hotmail.com>
2020-10-08 16:01:26 +0800

    add(backtracing): binary_tree_path

---
+++ src/backtracing/binary_tree_paths.go
@@ -0,0 +1,39 @@
+package backtracing
+
+import (
+	"go-leetcode/src/datastructure"
+	"strconv"
+	"strings"
+)
+
+func dfs(node *datastructure.TreeNode, elem []int, result *[]string) {
+	if node.Left == nil && node.Right == nil {
+		elem = append(elem, node.Val)
+		s := make([]string, 0)
+		for _, item := range elem {
+			s = append(s, strconv.Itoa(item))
+		}
+		ss := strings.Join(s, "->")
+		*result = append(*result, ss)
+		elem = elem[:len(elem)-1]
+		return
+	}
+	elem = append(elem, node.Val)
+	if node.Left != nil {
+		dfs(node.Left, elem, result)
+	}
+	if node.Right != nil {
+		dfs(node.Right, elem, result)
+	}
+	elem = elem[:len(elem)-1]
+}
+
+func binaryTreePaths(root *datastructure.TreeNode) []string {
+	if root == nil {
+		return []string{}
+	}
+	result := make([]string, 0)
+	elem := make([]int, 0)
+	dfs(root, elem, &result)
+	return result
+}
