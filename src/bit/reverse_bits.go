062c9d0bbce3202d99af09e0a17e11b9f3cd9306
ray1888 <alal1995@hotmail.com>
2020-10-30 22:53:01 +0800

    use add to clear

--- src/bit/reverse_bits.go
+++ src/bit/reverse_bits.go
@@ -1,11 +1,11 @@
 package bit
 
 func reverseBits(num uint32) uint32 {
-	power, ret := uint32(31), uint32(0)
-	for num != 0 {
-		ret += (num & 1) << power
-		num = num >> 1
-		power--
+	var result uint32
+	var i uint32
+	for ; i < 32; i++ {
+		result = (result << 1) | (num & 1)
+		num >>= 1
 	}
-	return ret
+	return result
 }
