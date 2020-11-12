39512bc283dd564d36f064d58a96d91f562a44be
ray1888 <alal1995@hotmail.com>
2020-11-12 22:00:08 +0800

    add(easy):range saum query

---
+++ src/numbers/range-sum-query-immutable.go
@@ -0,0 +1,24 @@
+package numbers
+
+type NumArray struct {
+	PrefixSum []int
+}
+
+func Constructor(nums []int) NumArray {
+	n := NumArray{}
+	n.PrefixSum = make([]int, len(nums)+1)
+	for i := 1; i < len(n.PrefixSum); i++ {
+		n.PrefixSum[i] = n.PrefixSum[i-1] + nums[i-1]
+	}
+	return n
+}
+
+func (this *NumArray) SumRange(i int, j int) int {
+	return this.PrefixSum[j+1] - this.PrefixSum[i]
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * obj := Constructor(nums);
+ * param_1 := obj.SumRange(i,j);
+ */
