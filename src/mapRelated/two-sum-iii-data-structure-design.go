1100ae010f34b92ba96300a21477bfb384e318c2
ray1888 <alal1995@hotmail.com>
2020-11-17 22:00:16 +0800

    add(easy): two sum data structure design

---
+++ src/mapRelated/two-sum-iii-data-structure-design.go
@@ -0,0 +1,18 @@
+package mapRelated
+
+type TwoSumList struct {
+	data  []int
+	index map[int]bool
+}
+
+func (ts *TwoSumList) Add(a int) {
+	for _, item := range ts.data {
+		ts.index[item+a] = true
+	}
+	ts.data = append(ts.data, item)
+}
+
+func (ts *TwoSumList) Find(k int) bool {
+	_, ok := ts.index[k]
+	return ok
+}
