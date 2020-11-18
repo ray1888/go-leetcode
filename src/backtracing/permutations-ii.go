8f6bcdc5861fb7b463e0a713ef475884bbb89c3d
ray1888 <alal1995@hotmail.com>
2020-11-18 22:24:34 +0800

    add(meidum): permuteUnique

---
+++ src/backtracing/permutations-ii.go
@@ -0,0 +1,29 @@
+package backtracing
+
+import "sort"
+
+func permuteUnique(nums []int) (ans [][]int) {
+	sort.Ints(nums)
+	n := len(nums)
+	perm := []int{}
+	vis := make([]bool, n)
+	var backtrack func(int)
+	backtrack = func(idx int) {
+		if idx == n {
+			ans = append(ans, append([]int(nil), perm...))
+			return
+		}
+		for i, v := range nums {
+			if vis[i] || i > 0 && !vis[i-1] && v == nums[i-1] {
+				continue
+			}
+			perm = append(perm, v)
+			vis[i] = true
+			backtrack(idx + 1)
+			vis[i] = false
+			perm = perm[:len(perm)-1]
+		}
+	}
+	backtrack(0)
+	return
+}
