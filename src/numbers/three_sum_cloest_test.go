2e281918e77ae720a9b8d9385d67224d09f7876d
ray1888 <alal1995@hotmail.com>
2020-12-04 22:55:13 +0800

    change test format

--- src/numbers/three_sum_cloest_test.go
+++ src/numbers/three_sum_cloest_test.go
@@ -13,18 +13,31 @@ type threeSumTest struct {
 
 func TestThreeSumCloest(t *testing.T) {
 	tests := []threeSumTest{
-		{Name: "add as target", Input: struct {
+		{
+			Name: "add as target", Input: struct {
 			array  []int
 			target int
-		}{array: []int{-5, 1, 3, 4, 5}, target: 1}, Output: 1},
-		{Name: "add not as target", Input: struct {
+		}{
+			array: []int{-5, 1, 3, 4, 5}, 
+			target: 1}, 
+			Output: 1
+		},
+		{
+			Name: "add not as target", Input: struct {
 			array  []int
 			target int
-		}{array: []int{-3, 1, 3, 4, 5}, target: -1}, Output: 0},
-		{Name: "add in seq", Input: struct {
+		}{
+			array: []int{-3, 1, 3, 4, 5}, 
+			target: -1}, 
+			Output: 0},
+		{
+			Name: "add in seq", Input: struct {
 			array  []int
 			target int
-		}{array: []int{0, 1, 2}, target: 3}, Output: 3},
+		}{
+			array: []int{0, 1, 2}, 
+			target: 3}, 
+			Output: 3},
 	}
 	for _, test := range tests {
 		result := ThreeSumCloest(test.Input.array, test.Input.target)
