632f23bd7f2e47c38da435dd80b91dd177b40645
ray1888 <alal1995@hotmail.com>
2020-12-14 22:10:09 +0800

    integer-break

---
+++ src/DynamicProgramming/integer-break_test.go
@@ -0,0 +1,29 @@
+package DynamicProgramming
+
+import "testing"
+
+func Test_integerBreak(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			// 10 = 3+ 3+ 4 , 3 * 3 * 4 =36
+			args: args{
+				n: 10,
+			},
+			want: 36,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := integerBreak(tt.args.n); got != tt.want {
+				t.Errorf("integerBreak() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
