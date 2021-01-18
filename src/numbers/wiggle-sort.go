657b474b5ab60d6ca022a2dfa58acedd33bdac1f
ray1888 <alal1995@hotmail.com>
2021-01-18 21:44:51 +0800

    add: wigglesort

---
+++ src/numbers/wiggle-sort.go
@@ -0,0 +1,17 @@
+import "sort"
+
+func wiggleSort(nums []int) {
+	if len(nums) == 0 {
+		return
+	}
+	sort.Ints(nums)
+	for i := 1; i < len(nums)-1; i += 2 {
+		swap(nums, i, i+1)
+	}
+}
+
+func swap(arr []int, i, j int) {
+	tmp := arr[i]
+	arr[i] = arr[j]
+	arr[j] = tmp
+}
