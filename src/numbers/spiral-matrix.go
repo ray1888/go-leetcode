a595cba3f160aa3107cf2e8054bd708b2936f29c
ray1888 <alal1995@hotmail.com>
2021-01-22 20:55:22 +0800

    add: spiral-matrix

---
+++ src/numbers/spiral-matrix.go
@@ -0,0 +1,48 @@
+package numbers
+
+func spiralOrder(matrix [][]int) []int {
+	if len(matrix) == 0 {
+		return []int{}
+	}
+
+	left := 0
+	right := len(matrix[0]) - 1
+	top := 0
+	bottom := len(matrix) - 1
+	result := make([]int, 0)
+
+	for true {
+		for i := left; i <= right; i++ {
+			result = append(result, matrix[top][i])
+		}
+		top++
+		if top > bottom {
+			break
+		}
+
+		for i := top; i <= bottom; i++ {
+			result = append(result, matrix[i][right])
+		}
+		right--
+		if left > right {
+			break
+		}
+
+		for i := right; i >= left; i-- {
+			result = append(result, matrix[bottom][i])
+		}
+		bottom--
+		if top > bottom {
+			break
+		}
+
+		for i := bottom; i >= top; i-- {
+			result = append(result, matrix[i][left])
+		}
+		left++
+		if left > right {
+			break
+		}
+	}
+	return result
+}
---
+++ src/numbers/spiral-matrix_test.go
@@ -0,0 +1,49 @@
+package numbers
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_spiralOrder(t *testing.T) {
+	type args struct {
+		matrix [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			args:args{
+				matrix:[][]int{
+					[]int{1, 2, 3},
+					[]int{4, 5, 6},
+					[]int{7, 8, 9},
+				}
+			},
+			want: []int{
+				1,2,3,6,9,8,7,4,5
+			},
+		},
+		{
+			args:args{
+				matrix:[][]int{
+					[]int{1, 2, 3, 4},
+					[]int{5, 6, 7, 8},
+					[]int{9,10,11,12},
+				}
+			},
+			want: []int{
+				1,2,3,4,8,12,11,10,9,5,6,7
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := spiralOrder(tt.args.matrix); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("spiralOrder() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
