6b12858c3498aac8f75441d7bf1cbfca1212a2ee
ray1888 <alal1995@hotmail.com>
2021-01-25 20:20:54 +0800

    add: lru solution

---
+++ src/lru/lru_double_link_list_seperate.go
@@ -0,0 +1,90 @@
+package lru
+
+type LRUCache2 struct {
+	size     int
+	capacity int
+	cache    map[int]*DLinkedNode
+	// 双向链表首尾不相接
+	head, tail *DLinkedNode
+}
+
+type DLinkedNode struct {
+	key, value int
+	prev, next *DLinkedNode
+}
+
+func initDLinkedNode(key, value int) *DLinkedNode {
+	return &DLinkedNode{
+		key:   key,
+		value: value,
+	}
+}
+
+func Constructor(capacity int) LRUCache2 {
+	l := LRUCache2{
+		cache:    map[int]*DLinkedNode{},
+		head:     initDLinkedNode(0, 0),
+		tail:     initDLinkedNode(0, 0),
+		capacity: capacity,
+	}
+	l.head.next = l.tail
+	l.tail.prev = l.head
+	return l
+}
+
+func (this *LRUCache2) Get(key int) int {
+	if _, ok := this.cache[key]; !ok {
+		return -1
+	}
+	node := this.cache[key]
+	this.moveToHead(node)
+	return node.value
+}
+
+func (this *LRUCache2) Put(key int, value int) {
+	if _, ok := this.cache[key]; !ok {
+		node := initDLinkedNode(key, value)
+		this.cache[key] = node
+		this.addToHead(node)
+		this.size++
+		if this.size > this.capacity {
+			removed := this.removeTail()
+			delete(this.cache, removed.key)
+			this.size--
+		}
+	} else {
+		node := this.cache[key]
+		node.value = value
+		this.moveToHead(node)
+	}
+}
+
+func (this *LRUCache2) addToHead(node *DLinkedNode) {
+	node.prev = this.head
+	node.next = this.head.next
+	this.head.next.prev = node
+	this.head.next = node
+}
+
+func (this *LRUCache2) removeNode(node *DLinkedNode) {
+	node.prev.next = node.next
+	node.next.prev = node.prev
+}
+
+func (this *LRUCache2) moveToHead(node *DLinkedNode) {
+	this.removeNode(node)
+	this.addToHead(node)
+}
+
+func (this *LRUCache2) removeTail() *DLinkedNode {
+	node := this.tail.prev
+	this.removeNode(node)
+	return node
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * obj := Constructor(capacity);
+ * param_1 := obj.Get(key);
+ * obj.Put(key,value);
+ */
--- src/lru/lru_test.go
+++ src/lru/lru_test.go
@@ -55,3 +55,16 @@ func TestLRUSaveGetOverLength(t *testing.T) {
 		}
 	}
 }
+
+func Test_leetcodeUseCase(t *testing.T) {
+	lru, _ := NewLRUCache(2)
+	lru.Put(1, 1)
+	lru.Put(2, 2)
+	lru.Get(1)
+	lru.Put(3, 3)
+	lru.Get(2)
+	lru.Put(4, 4)
+	lru.Get(1)
+	lru.Get(3)
+	lru.Get(4)
+}
