b5a03be673283271f31ee2d99939788a64a9ba05
ray1888 <alal1995@hotmail.com>
2021-02-01 16:18:25 +0800

    subarraySum

---
+++ src/DynamicProgramming/subarray-sum-equals-k.go
@@ -0,0 +1,15 @@
+package DynamicProgramming
+
+func subarraySum2(nums []int, k int) int {
+	count, pre := 0, 0
+	m := map[int]int{}
+	m[0] = 1
+	for i := 0; i < len(nums); i++ {
+		pre += nums[i]
+		if _, ok := m[pre-k]; ok {
+			count += m[pre-k]
+		}
+		m[pre] += 1
+	}
+	return count
+}
