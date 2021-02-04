cb6783dc7bcf383e3abe9ad850126e106667da22
ray1888 <alal1995@hotmail.com>
2021-02-04 10:49:01 +0800

    add: longestparlidromesubstring

---
+++ src/DynamicProgramming/longest-palindromic-substring.go
@@ -0,0 +1,33 @@
+package DynamicProgramming
+
+func longestPalindrome(s string) string {
+	if len(s) == 0 {
+		return ""
+	}
+	length := len(s)
+	dp := make([][]bool, length)
+
+	for i := 0; i < length; i++ {
+		dp[i] = make([]bool, length)
+	}
+
+	start := 0
+	maxLength := 0
+
+	for i := length - 1; i >= 0; i-- {
+		for j := i; j < length; j++ {
+			if i == j {
+				dp[i][j] = true
+			} else if i+1 == j {
+				dp[i][j] = (s[i] == s[j])
+			} else {
+				dp[i][j] = (s[i] == s[j] && dp[i+1][j-1])
+			}
+			if dp[i][j] && (j-i+1 > maxLength) {
+				start = i
+				maxLength = j - i + 1
+			}
+		}
+	}
+	return s[start : start+maxLength]
+}
---
+++ src/DynamicProgramming/longest-palindromic-substring_test.go
@@ -0,0 +1,34 @@
+package DynamicProgramming
+
+import "testing"
+
+func Test_longestPalindrome(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			args: args{
+				s: "babad",
+			},
+			want: "aba",
+		},
+		{
+			args: args{
+				s: "cbbd",
+			},
+			want: "bb",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestPalindrome(tt.args.s); got != tt.want {
+				t.Errorf("longestPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
