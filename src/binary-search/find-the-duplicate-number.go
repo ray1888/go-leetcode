8b919a4de4931d6b5f5d5bd2aa9e9ee7a0652b7d
ray1888 <alal1995@hotmail.com>
2021-02-19 17:09:27 +0800

    add: find duplicate number

---
+++ src/binary-search/find-the-duplicate-number.go
@@ -0,0 +1,23 @@
+package binary_search
+
+func findDuplicate(nums []int) int {
+	n := len(nums)
+	l, r := 1, n-1
+	ans := -1
+	for l <= r {
+		mid := (l + r) >> 1
+		cnt := 0
+		for i := 0; i < n; i++ {
+			if nums[i] <= mid {
+				cnt++
+			}
+		}
+		if cnt <= mid {
+			l = mid + 1
+		} else {
+			r = mid - 1
+			ans = mid
+		}
+	}
+	return ans
+}
---
+++ src/binary-search/find-the-duplicate-number_test.go
@@ -0,0 +1,28 @@
+package binary_search
+
+import "testing"
+
+func Test_findDuplicate(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			args: args{
+				nums: []int{1, 3, 4, 2, 2},
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findDuplicate(tt.args.nums); got != tt.want {
+				t.Errorf("findDuplicate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
