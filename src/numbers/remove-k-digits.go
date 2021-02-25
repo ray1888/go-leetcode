2ae2cf3e772fcaa33352715d32e7226f5fe13c94
ray1888 <alal1995@hotmail.com>
2021-02-25 17:21:37 +0800

    add: remove k digits

---
+++ src/numbers/remove-k-digits.go
@@ -0,0 +1,21 @@
+package numbers
+
+import "strings"
+
+func removeKdigits(num string, k int) string {
+	stack := []byte{}
+	for i := range num {
+		digit := num[i]
+		for k > 0 && len(stack) > 0 && digit < stack[len(stack)-1] {
+			stack = stack[:len(stack)-1]
+			k--
+		}
+		stack = append(stack, digit)
+	}
+	stack = stack[:len(stack)-k]
+	ans := strings.TrimLeft(string(stack), "0")
+	if ans == "" {
+		ans = "0"
+	}
+	return ans
+}
---
+++ src/numbers/remove-k-digits_test.go
@@ -0,0 +1,37 @@
+package numbers
+
+import "testing"
+
+func Test_removeKdigits(t *testing.T) {
+	type args struct {
+		num string
+		k   int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			args: args{
+				num: "1432219",
+				k:   3,
+			},
+			want: "1219",
+		},
+		{
+			args: args{
+				num: "10200",
+				k:   1,
+			},
+			want: "200",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeKdigits(tt.args.num, tt.args.k); got != tt.want {
+				t.Errorf("removeKdigits() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
