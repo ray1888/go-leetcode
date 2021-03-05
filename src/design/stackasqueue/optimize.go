10ed07c601b230812ba4eaf2f0ed5aaf5d91711a
ray1888 <alal1995@hotmail.com>
2021-03-05 09:20:53 +0800

    add: optimize version of stack as queue

---
+++ src/design/stackasqueue/optimize.go
@@ -0,0 +1,49 @@
+package stackasqueue
+
+type MyQQueue struct {
+	inStack, outStack []int
+}
+
+func Constructor2() MyQQueue {
+	return MyQQueue{}
+}
+
+func (q *MyQQueue) Push(x int) {
+	q.inStack = append(q.inStack, x)
+}
+
+func (q *MyQQueue) in2out() {
+	for len(q.inStack) > 0 {
+		q.outStack = append(q.outStack, q.inStack[len(q.inStack)-1])
+		q.inStack = q.inStack[:len(q.inStack)-1]
+	}
+}
+
+func (q *MyQQueue) Pop() int {
+	if len(q.outStack) == 0 {
+		q.in2out()
+	}
+	x := q.outStack[len(q.outStack)-1]
+	q.outStack = q.outStack[:len(q.outStack)-1]
+	return x
+}
+
+func (q *MyQQueue) Peek() int {
+	if len(q.outStack) == 0 {
+		q.in2out()
+	}
+	return q.outStack[len(q.outStack)-1]
+}
+
+func (q *MyQQueue) Empty() bool {
+	return len(q.inStack) == 0 && len(q.outStack) == 0
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * param_2 := obj.Pop();
+ * param_3 := obj.Peek();
+ * param_4 := obj.Empty();
+ */
