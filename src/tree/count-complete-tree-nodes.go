34c30dd9733d1136b0ac10a193d22893a2c49ce8
ray1888 <alal1995@hotmail.com>
2021-03-06 21:07:10 +0800

    add: count complete tree nodes

---
+++ src/tree/count-complete-tree-nodes.go
@@ -0,0 +1,40 @@
+package tree
+
+import (
+	"go-leetcode/src/datastructure"
+	"sort"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func countNodes(root *datastructure.TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	level := 0
+	for node := root; node.Left != nil; node = node.Left {
+		level++
+	}
+	return sort.Search(1<<(level+1), func(k int) bool {
+		if k <= 1<<level {
+			return false
+		}
+		bits := 1 << (level - 1)
+		node := root
+		for node != nil && bits > 0 {
+			if bits&k == 0 {
+				node = node.Left
+			} else {
+				node = node.Right
+			}
+			bits >>= 1
+		}
+		return node == nil
+	}) - 1
+}
