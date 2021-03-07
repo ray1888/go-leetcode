05b050595e4294ffe9d429abe5bcd7df268eab48
ray1888 <alal1995@hotmail.com>
2021-03-07 22:02:32 +0800

    add: next greter element ii

---
+++ src/numbers/maxnumber/next-greater-element-ii_test.go
@@ -0,0 +1,31 @@
+package maxnumber
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_nextGreaterElements(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			args: args{
+				nums: []int{1, 2, 1},
+			},
+			want: []int{2, -1, 2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := nextGreaterElements(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("nextGreaterElements() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
