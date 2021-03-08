0b1fb82edffdffe3483b94bd7172ca1c49cf566a
ray1888 <alal1995@hotmail.com>
2021-03-08 17:02:48 +0800

    add: decode string iterative version

--- src/stringProblem/decode-string.go
+++ src/stringProblem/decode-string.go
@@ -1,6 +1,9 @@
 package stringProblem
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+)
 
 var (
 	src string
@@ -37,6 +40,53 @@ func getDigits() int {
 	ret := 0
 	for ; src[ptr] >= '0' && src[ptr] <= '9'; ptr++ {
 		ret = ret*10 + int(src[ptr]-'0')
+	}
+	return ret
+}
+
+func getStringIterative(s string) string {
+	stk := []string{}
+	ptr := 0
+	for ptr < len(s) {
+		cur := s[ptr]
+		if cur >= '0' && cur <= '9' {
+			digits := getDigitsIter(s, &ptr)
+			stk = append(stk, digits)
+		} else if (cur >= 'a' && cur <= 'z' || cur >= 'A' && cur <= 'Z') || cur == '[' {
+			stk = append(stk, string(cur))
+			ptr++
+		} else {
+			ptr++
+			sub := []string{}
+			for stk[len(stk)-1] != "[" {
+				sub = append(sub, stk[len(stk)-1])
+				stk = stk[:len(stk)-1]
+			}
+			for i := 0; i < len(sub)/2; i++ {
+				sub[i], sub[len(sub)-i-1] = sub[len(sub)-i-1], sub[i]
+			}
+			stk = stk[:len(stk)-1]
+			repTime, _ := strconv.Atoi(stk[len(stk)-1])
+			stk = stk[:len(stk)-1]
+			t := strings.Repeat(getSubString(sub), repTime)
+			stk = append(stk, t)
+		}
+	}
+	return getSubString(stk)
+}
+
+func getDigitsIter(s string, ptr *int) string {
+	ret := ""
+	for ; s[*ptr] >= '0' && s[*ptr] <= '9'; *ptr++ {
+		ret += string(s[*ptr])
+	}
+	return ret
+}
+
+func getSubString(v []string) string {
+	ret := ""
+	for _, s := range v {
+		ret += s
 	}
 	return ret
 }
