847f106c7770cef7c67342ea13b134a1feb9adaf
ray1888 <alal1995@hotmail.com>
2021-03-13 20:27:54 +0800

    add: rotate image

---
+++ src/numbers/rotate-image.go
@@ -0,0 +1,15 @@
+package numbers
+
+func rotate(matrix [][]int) {
+	// 水平翻转
+	n := len(matrix)
+	for i := 0; i < n/2; i++ {
+		matrix[i], matrix[n-1-i] = matrix[n-1-i], matrix[i]
+	}
+	// 主对角线翻转
+	for i := 0; i < n; i++ {
+		for j := 0; j < i; j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+}
---
+++ src/numbers/rotate-image_test.go
@@ -0,0 +1,34 @@
+package numbers
+
+import "testing"
+
+func Test_rotate(t *testing.T) {
+	type args struct {
+		matrix [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			args: args{
+				matrix: [][]int{
+					{1, 2, 3},
+					{4, 5, 6},
+					{7, 8, 9},
+				},
+			},
+			want: [][]int{
+				{7, 4, 1},
+				{8, 5, 2},
+				{9, 6, 3},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rotate(tt.args.matrix)
+		})
+	}
+}
